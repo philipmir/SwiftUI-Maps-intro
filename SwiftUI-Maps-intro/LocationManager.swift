779484ccccccdc73183338ac3147bcc5209aed6f
philipmir <philipmir@hotmail.com>
2023-05-01 15:55:16 +0200

    first commit

---
+++ SwiftUI-Maps-intro/LocationManager.swift
@@ -0,0 +1,8 @@
+//
+//  LocationManager.swift
+//  SwiftUI-Maps-intro
+//
+//  Created by Philip Miranda on 2023-05-01.
+//
+
+import Foundation
---
+++ SwiftUI-Maps-intro/Place.swift
@@ -0,0 +1,8 @@
+//
+//  Place.swift
+//  SwiftUI-Maps-intro
+//
+//  Created by Philip Miranda on 2023-05-01.
+//
+
+import Foundation
