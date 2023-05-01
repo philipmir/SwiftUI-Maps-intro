7cc8e56d7641a7e65a72c4a3730c1933bb4d72f4
philipmir <philipmir@hotmail.com>
2023-05-01 14:59:39 +0200

    Initial Commit

---
+++ SwiftUI-Maps-intro/SwiftUI_Maps_introApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Maps_introApp.swift
+//  SwiftUI-Maps-intro
+//
+//  Created by Philip Miranda on 2023-05-01.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Maps_introApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
